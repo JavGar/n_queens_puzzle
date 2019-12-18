8ecc838f892d3bfdce2cc58383b9885bc6fd4f9e
JavGar <dushtajavier@hotmail.com>
2019-12-18 13:29:16 -0600

    Start project

---
+++ android/app/src/main/kotlin/cuenca/hjgc/n_quees_puzzle/MainActivity.kt
@@ -0,0 +1,12 @@
+package cuenca.hjgc.n_quees_puzzle
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
