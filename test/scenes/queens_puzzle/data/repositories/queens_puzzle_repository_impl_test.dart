2304683a9da76be8fbe1560a9c84929b050b0052
JavGar <dushtajavier@hotmail.com>
2019-12-19 15:22:13 -0600

    Update test to use new model

--- test/scenes/queens_puzzle/data/repositories/queens_puzzle_repository_impl_test.dart
+++ test/scenes/queens_puzzle/data/repositories/queens_puzzle_repository_impl_test.dart
@@ -15,21 +15,21 @@ main() {
       // act
       final result1 = await repository.getBoardSolutions(tN[0]);
       // assert
-      expect(result1.length, equals(2));
+      expect(result1.solutions.length, equals(2));
     });
 
     test('Should return list of "boards" as solutions for board size 5', () async {
       // act
       final result2 = await repository.getBoardSolutions(tN[1]);
       // assert
-      expect(result2.length, equals(10));
+      expect(result2.solutions.length, equals(10));
     });
 
     test('Should return list of "boards" as solutions for board size 6', () async {
       // act
       final result3 = await repository.getBoardSolutions(tN[2]);
       // assert
-      expect(result3.length, equals(4));
+      expect(result3.solutions.length, equals(4));
     });
   });
 }
--- test/scenes/queens_puzzle/presentation/bloc/queens_puzzle_bloc_test.dart
+++ test/scenes/queens_puzzle/presentation/bloc/queens_puzzle_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:n_quees_puzzle/scenes/queens_puzzle/domain/models/board_solution_model.dart';
 import 'package:n_quees_puzzle/scenes/queens_puzzle/domain/usecases/get_board_solutions.dart';
 import 'package:n_quees_puzzle/scenes/queens_puzzle/presentation/bloc/queens_puzzle_bloc.dart';
 import 'package:n_quees_puzzle/scenes/queens_puzzle/presentation/bloc/queens_puzzle_event.dart';
@@ -26,13 +27,14 @@ main() {
     final List s1 = [1, 3, 0, 2];
     final List s2 = [2, 0, 3, 1];
     final List<List> sb4 = [s1, s2];
+    final BoardSolutionModel tBoardSolutionModel = BoardSolutionModel(sb4, "dummy time");
 
     void setUpMockGetBoardSolutions() =>
-        when(mockGetBoardSolutions(any)).thenAnswer((_) async => sb4);
+        when(mockGetBoardSolutions(any)).thenAnswer((_) async => tBoardSolutionModel);
 
     test('Should get calculated solutions', () async {
       // arrange
-      when(mockGetBoardSolutions(any)).thenAnswer((_) async => sb4);
+      setUpMockGetBoardSolutions();
       // act
       bloc.add(CalculateSolutions(tN));
       await untilCalled(mockGetBoardSolutions(any));
@@ -53,14 +55,14 @@ main() {
           emitsInOrder([
             InitialQueensPuzzleState(),
             LoadingQueensPuzzleState(),
-            LoadedQueensPuzzleState(sb4),
+            LoadedQueensPuzzleState(tBoardSolutionModel),
           ]));
     });
 
     blocTest(
       'Should emit [InitialQueensPuzzleState, LoadingQueensPuzzleState, ErrorQueensPuzzleState] when board hasn\'t solutions (Using bloc_test)',
       build: () {
-        when(mockGetBoardSolutions(any)).thenAnswer((_) async => List());
+        when(mockGetBoardSolutions(any)).thenAnswer((_) async => BoardSolutionModel(List(), "dummy time"));
         return bloc;
       },
       act: (bloc) => bloc.add(CalculateSolutions(tN)),
